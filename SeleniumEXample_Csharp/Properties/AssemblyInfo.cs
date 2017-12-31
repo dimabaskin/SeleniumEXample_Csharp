e989065a1809c2367ab421c51b533e3aafbd352a
dimabaskin <dimabaskin@gmai.com>
2017-12-31 14:47:02 +0200

    Software Testing ru Selenium Project
    Exercises

---
+++ SeleniumEXample_Csharp/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SeleniumEXample_Csharp")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SeleniumEXample_Csharp")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c1fd41b9-f11e-470f-b232-1fe3f7c5ffbb")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SeleniumEXample_Csharp/SimpleTest.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.IE;
+using OpenQA.Selenium.Support.UI;
+
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SeleniumEXample_Csharp
+{
+    [TestFixture]
+    public class SimpleTest
+    {
+
+        private IWebDriver driver;
+        private WebDriverWait whait;
+        
+        [SetUp]
+        public void start()
+        {
+            driver = new ChromeDriver();
+            whait = new WebDriverWait(driver, TimeSpan.FromSeconds(15));
+
+        }
+
+
+        [Test]
+        public void OpenBrowser()
+        {
+            driver.Url = "http://software-testing.ru/";
+            //driver.FindElement(By.Name("q")).SendKeys("software-testing.ru" + Keys.Enter);
+            //driver.FindElement(By.LinkText("http://software-testing.ru/")).Click();
+            whait.Until(ExpectedConditions.TitleIs("Software-Testing.Ru"));
+        }
+
+        [TearDown]
+        public void stop()
+        {
+            driver.Quit();
+            driver = null;
+        }
+    }
+}
